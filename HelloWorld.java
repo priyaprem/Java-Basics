ad598f407be88761e9384d964214ff55547d9563
priyaprem <37586565+priyaprem@users.noreply.github.com>
2018-07-19 11:50:15 +0530

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+//A SIMPLE HELLO WORLD PROGRAM
+// optional import package(import java.io.*)
+import java.io.*
+public class Sample
+{
+  public static void main(String args[])
+  {
+    System.out.println("HELLO WORLD");
+  }
+}
